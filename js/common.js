8c46ba47b1217d111dbb5c386be1af1ad231b954
Space <space@471.no>
2018-11-08 01:56:12 +0100

    General cleanup, more placeholder products, load header/footer from file

--- js/common.js
+++ js/common.js
@@ -1,37 +1,56 @@
+/* 
+	images are licensed under creative commons.
+	names are (mostly) randomly generated. 
+*/
+
 const products = [
 	{
 		id: 1,
-		name: "A thing",
-		price: 900,
-		img: "strawberry-banana.jpg",
+		name: "Spearmint Crystal",
+		price: 120,
+		img: "green-drink.jpg",
 		category: "Smoothie"
 	},
 	{
 		id: 2,
-		name: "Another thing",
-		price: 900,
-		img: "strawberry-banana.jpg",
+		name: "River Wave",
+		price: 107,
+		img: "yellow-drink.jpg",
 		category: "Smoothie"
 	},
 	{
 		id: 3,
-		name: "A thing",
-		price: 900,
+		name: "Arctic Walk",
+		price: 39,
 		img: "strawberry-banana.jpg",
 		category: "Smoothie"
 	},
 	{
 		id: 4,
-		name: "Another thing IIIIIIASSIIIIdsasdad",
-		price: 900,
-		img: "strawberry-banana.jpg",
+		name: "Gentle Blizzard",
+		price: 49,
+		img: "orange-drink.jpg",
 		category: "Smoothie"
 	},
 	{
 		id: 5,
-		name: "A thing",
-		price: 900,
-		img: "nice-bread.png",
+		name: "Our Bread",
+		price: 60,
+		img: "rye-bread.jpg",
+		category: "Bread"
+	},
+	{
+		id: 6,
+		name: "Wet Sour",
+		price: 40,
+		img: "bread-rolls-2.jpg",
+		category: "Bread"
+	},
+	{
+		id: 7,
+		name: "Rough Touch",
+		price: 471,
+		img: "bread-rolls.jpg",
 		category: "Bread"
 	}
 ];
@@ -48,15 +67,17 @@ const dialogTemplateHtml = '' +
 var cart = [];
 
 function storeCart() {
-	// stores cart to cookie
+	/* stores cart to cookie */
 	var cartString = JSON.stringify(cart);
 	console.log(cartString);
 	document.cookie = "cart="+btoa(cartString)+";";
 }
 
 function getCookie(key) {
-    // helper: gets cookie with input key
-    // source: https://stackoverflow.com/questions/10730362/get-cookie-by-name
+	/* 
+		helper: gets cookie with input key
+		source: https://stackoverflow.com/questions/10730362/get-cookie-by-name 
+	*/
 	var value = "; " + document.cookie;
 	var parts = value.split("; " + key + "=");
 	if (parts.length == 2) {
@@ -65,7 +86,7 @@ function getCookie(key) {
 }
 
 function loadCart() {
-	// loads cart from stored cookie
+	/* loads cart from stored cookie */
 	var cartCookie = getCookie("cart");
 	if (cartCookie) {
 		var cartString = atob(getCookie("cart"));
@@ -77,6 +98,7 @@ function loadCart() {
 }
 
 function getTotalItemsInCart() {
+	/* returns the total amount of items in the cart. */
     var itemCount = 0;
     for (var i = 0; i < cart.length; i++) {
         itemCount += cart[i].count;
@@ -92,6 +114,7 @@ function updateCartCounter() {
 }
 
 function emptyCart() {
+	/* clears the cart variable and updates the cart if it is open. */
 	cart = [];
 	if (document.querySelector(".dialog-visible .cart")) {
 		renderCart();
@@ -100,11 +123,20 @@ function emptyCart() {
     updateCartCounter();
 }
 
-function openCheckout() {
+function renderCheckoutToString() {
+	return "";
+}
+
+function showCheckoutSuccessDialog() {
+	return "";
+}
+
+function showCheckoutDialog() {
     showDialog("Checkout", '<p>Checkout?</p>');
 }
 
 function getCartItemCount(itemId) {
+	/* gets the amount we have of an item, if we have it in the cart. */
 	var item = getItemData(itemId, cart);
 	if (item) {
 		return item.count;
@@ -113,6 +145,7 @@ function getCartItemCount(itemId) {
 }
 
 function getItemData(itemId, target) {
+	/* gets the data associated with a product stored in our products list */
 	var item = target.filter(e => e.id === itemId);
 	if (item.length > 0) {
 		return item[0];
@@ -121,7 +154,7 @@ function getItemData(itemId, target) {
 }
 
 function renderCart() {
-	// renders cart from cart variable
+	/* renders cart from cart variable */
 	const cart_container = document.querySelector(".cart");
 	cart_container.innerHTML = "<p>Your cart is empty.</p>";
 	if (cart.length > 0) {
@@ -142,8 +175,8 @@ function renderCart() {
     var emptyCartButtons = dialog.getElementsByClassName("empty-cart-btn");
 
     for (var i = 0; i < checkoutButtons.length; i++) {
-        checkoutButtons[i].removeEventListener('click', openCheckout);
-        checkoutButtons[i].addEventListener('click', openCheckout);
+        checkoutButtons[i].removeEventListener('click', showCheckoutDialog);
+        checkoutButtons[i].addEventListener('click', showCheckoutDialog);
     }
 
     for (var i = 0; i < emptyCartButtons.length; i++) {
@@ -153,6 +186,7 @@ function renderCart() {
 }
 
 function showEmptyCartDialog() {
+	/* ask the user to confirm that they want to empty their cart */
 	showDialog("Confirm", '<p>Are you sure you wish to empty your cart?</p>', '<button onclick="emptyCart();hideDialog();">Yes, empty my cart</button><button class="close-dialog-btn">Cancel</button>');
 }
 
@@ -165,6 +199,14 @@ function hideDialog() {
 }
 
 function showDialog(title, contentHtml, footerHtml, onRenderCallback) {
+	/* 
+		shows a dialog window.
+		title: shows up in the title of the dialog.
+		contentHtml: any html, shows up in the dialog body.
+		footerHtml: any html, shows up in the footer. contains a close button by default.
+		onRenderCallback: function, runs after the dialog is finished rendering. useful for adding buttons.
+	*/
+
     var body = document.querySelector("body");
 
     if (!document.querySelector(".dialog-backdrop")) {
@@ -222,14 +264,45 @@ function showDialog(title, contentHtml, footerHtml, onRenderCallback) {
     dialog.classList.remove("dialog-hidden");
 }
 
+function renderFileToContainer(url, containerSelector, callback) {
+	/*
+		gets the contents of the .html file at the specified url,
+		and puts it in the container specified by the CSS selector
+		containerSelector. the callback function is run after that
+		is done. an usage example is at the bottom of this file.
+	*/
+
+	fetch("./"+url).then(function (response) {
+		return response.text();
+	}).then(function (html) {
+		document.querySelector(containerSelector).innerHTML = html;
+		if (callback) {
+			callback();
+		}
+	});
+}
+
+function renderFinishedCallback() {
+	/* code that we want to run after the header/footer has finished loading goes here */
+
+	/* bind cart button onclick to open cart, add cart item counters */
+	var openCartButtons = document.getElementsByClassName("show-cart-btn");
+	
+	for (var i = 0; i < openCartButtons.length; i++) {
+		openCartButtons[i].addEventListener('click', showCartDialog);
+
+		/* check whether button already has a cart item counter, add one if not */
+		if (!openCartButtons[i].querySelector(".cart-counter")) {
+			var cartCounter = document.createElement("span");
+			cartCounter.innerText = getTotalItemsInCart();
+			cartCounter.classList.add("cart-counter");
+			openCartButtons[i].appendChild(cartCounter);
+		}
+	}	
+}
+
 loadCart();
 
-var openCartButtons = document.getElementsByClassName("show-cart-btn");
-	
-for (var i = 0; i < openCartButtons.length; i++) {
-    openCartButtons[i].addEventListener('click', showCartDialog);
-    var cartCounter = document.createElement("span");
-    cartCounter.innerText = getTotalItemsInCart();
-    cartCounter.classList.add("cart-counter");
-    openCartButtons[i].appendChild(cartCounter);
-}
+renderFileToContainer("shared-html/header.html", "header", renderFinishedCallback);
+
+renderFileToContainer("shared-html/footer.html", "footer", renderFinishedCallback);
